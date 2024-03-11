5e7f794881ff264de61556ed8019520104d4b3b7
volkan.onder <volkan.onder@arksoft.com.tr>
2024-03-11 16:43:17 +0300

    primary ctor

---
+++ PrimaryConstructors/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+
+    public Product(int id, string name, decimal price)
+    {
+        Id = id;
+        Name = name;
+        Price = price;
+    }
+}
+
+public class ProductPrimaryCtor(int id, string name, decimal price)
+{
+    public int Id { get; private set; } = id;
+    public string Name { get; private set; } = name;
+    public decimal Price { get; private set; } = price;
+}
