e6dfb2fe58003d55cab99a787f25926d52f75ead
volkan.onder <volkan.onder@arksoft.com.tr>
2024-03-11 16:33:34 +0300

    getItems and Shuffle usage example

---
+++ NewMethodsForRandomness/Program.cs
@@ -0,0 +1,37 @@
+﻿GetItemsExample();
+Console.WriteLine("---------------------");
+ShuffleExample();
+
+static void GetItemsExample()
+{
+    string[] baroqueMusicians =
+        [
+            "Bach",
+            "Vivaldi",
+            "Corelli",
+            "Purcess",
+            "Monteverdi"
+        ];
+
+    foreach (var item in baroqueMusicians)
+    {
+        string[] randomChoices = Random.Shared.GetItems(baroqueMusicians, 2);
+
+        Console.WriteLine(string.Join(",", randomChoices));
+    }
+}
+
+static void ShuffleExample()
+{
+    string[] baroqueMusicians =
+        [
+            "Bach",
+            "Vivaldi",
+            "Corelli",
+            "Purcess",
+            "Monteverdi"
+        ];
+
+    Random.Shared.Shuffle(baroqueMusicians);
+    Console.WriteLine(string.Join("\n", baroqueMusicians));
+}
